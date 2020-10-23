f2dbac8cefe0745c5c02732d03eb62ce12e4d23a
yaseerairfan <57320789+yaseerairfan@users.noreply.github.com>
2020-10-22 23:19:21 -0700

    added a comment

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    //student info system
     <title>Student Information System</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
